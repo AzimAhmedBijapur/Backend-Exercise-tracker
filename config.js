eab43ac6cd64d98b7c6029eb41a2c163d2f596e9
ThunderBoltCodes <94512886+azimbijapur786@users.noreply.github.com>
2024-02-12 20:26:00 +0530

    environment vars

--- config.js
+++
@@ -1,2 +0,0 @@
-export const PORT=5000;
-export const mongoURL='mongodb+srv://azimahmed:Igloo8000@exercisestracker.peleplr.mongodb.net/';
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from 'express';
-import {PORT, mongoURL} from './config.js';
 import mongoose from 'mongoose';
 import router from './routes/routes.js';
 import cors from 'cors';
+import 'dotenv/config'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use('/',router);
 
-mongoose.connect(mongoURL)
+mongoose.connect(process.env.MONGOURL)
 .then(()=>{
     console.log('Connected to db');
-    app.listen(PORT,()=>{
+    app.listen(process.env.PORT,()=>{
         console.log('Listening to port 5000');
     })
 })
